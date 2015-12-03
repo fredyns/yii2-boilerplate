7e49f0ee12f06d4cdfe0b67e4db96df3dc5f78a2
fredy <fredy@fredy-Lenovo-B490>
2015-12-03 22:04:30 +0700

    intall yii2

---
+++ setpermission.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo chown -R fredy:www-data *
+sudo chmod -R 775 *  
+sudo chmod -R 777 backend/runtime
+sudo chmod -R 777 backend/web/assets
+sudo chmod -R 777 frontend/runtime
+sudo chmod -R 777 frontend/web/assets
+sudo chmod -R 775 yii
+sudo chmod -R 775 tests/codeception/bin/yii
+
