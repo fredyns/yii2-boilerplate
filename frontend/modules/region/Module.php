6d1fa27621e302866287f800721ba429957b73cd
fredy <fredy@fredy-Lenovo-B490>
2016-01-13 08:49:40 +0700

    init module region

---
+++ frontend/modules/region/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\modules\region;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'frontend\modules\region\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
