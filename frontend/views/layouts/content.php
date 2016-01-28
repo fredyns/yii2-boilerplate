6973c9b45c88ce9edca0d0d0f35469429818cf4c
fredy <fredy@fredy-Lenovo-B490>
2016-01-28 14:33:03 +0700

    move fb like to layout

--- frontend/views/layouts/content.php
+++ frontend/views/layouts/content.php
@@ -1,38 +1,73 @@
 <?php
+
 use yii\widgets\Breadcrumbs;
 use dmstr\widgets\Alert;
 
 ?>
 <div class="content-wrapper">
     <section class="content-header">
-        <?php if (isset($this->blocks['content-header'])) { ?>
-            <h1><?= $this->blocks['content-header'] ?></h1>
-        <?php } else { ?>
-            <h1>
-                <?php
-                if ($this->title !== null) {
-                    echo \yii\helpers\Html::encode($this->title);
-                } else {
-                    echo \yii\helpers\Inflector::camel2words(
-                        \yii\helpers\Inflector::id2camel($this->context->module->id)
-                    );
-                    echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
-                } ?>
-            </h1>
-        <?php } ?>
+		<?php
 
-        <?=
-        Breadcrumbs::widget(
-            [
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]
-        ) ?>
+		if (isset($this->blocks['content-header']))
+		{
+
+			?>
+			<h1><?= $this->blocks['content-header'] ?></h1>
+			<?php
+
+		}
+		else
+		{
+
+			?>
+			<h1>
+				<?php
+
+				if ($this->title !== null)
+				{
+					echo \yii\helpers\Html::encode($this->title);
+				}
+				else
+				{
+					echo \yii\helpers\Inflector::camel2words(
+						\yii\helpers\Inflector::id2camel($this->context->module->id)
+					);
+					echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
+				}
+
+				?>
+			</h1>
+		<?php } ?>
+
+		<?=
+
+		Breadcrumbs::widget(
+			[
+				'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+			]
+		)
+
+		?>
     </section>
 
     <section class="content">
-        <?= Alert::widget() ?>
-        <?= $content ?>
+		<?= Alert::widget() ?>
+		<?= $content ?>
     </section>
+
+	<div class="clearfix">
+
+		<div
+			class="fb-like"
+			data-share="true"
+			data-width="450"
+			data-show-faces="true"
+			style="float: right;"
+			>
+		</div>
+
+	</div>
+
 </div>
 
 <footer class="main-footer">
