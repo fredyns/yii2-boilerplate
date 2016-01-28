97f050187dcf751f036a7e82fee89d2969306e04
fredy <fredy@fredy-Lenovo-B490>
2016-01-28 14:23:00 +0700

    profile link

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -267,16 +267,10 @@ use yii\helpers\Html;
 							<!-- Menu Footer-->
 							<li class="user-footer">
 								<div class="pull-left">
-									<a href="#" class="btn btn-default btn-flat">Profile</a>
+									<?= Html::a('Profile', ['/user/settings/profile'], ['class' => 'btn btn-default btn-flat']) ?>
 								</div>
 								<div class="pull-right">
-									<?=
-
-									Html::a(
-										'Sign out', ['/user/security/logout'], ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
-									)
-
-									?>
+									<?= Html::a('Sign out', ['/user/security/logout'], ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']) ?>
 								</div>
 							</li>
 						</ul>
