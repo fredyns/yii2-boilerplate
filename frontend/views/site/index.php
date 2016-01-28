7904fbf2c3eba730705d9d0d0230393ab4f25b0f
fredy <fredy@fredy-Lenovo-B490>
2016-01-28 14:25:55 +0700

    move fb script to layout

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -199,24 +199,3 @@ $this->title = 'Yii 2 Boilerplate';
     </div>
 </div>
 
-
-<script>
-	window.fbAsyncInit = function () {
-		FB.init({
-			appId: '910117462416938',
-			xfbml: true,
-			version: 'v2.5'
-		});
-	};
-
-	(function (d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) {
-			return;
-		}
-		js = d.createElement(s);
-		js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-</script>
