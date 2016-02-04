6f4531727ef5b11736d97ee79ce4ab3c1a512dfc
fredy <fredy@fredy-Lenovo-B490>
2016-02-04 12:00:58 +0700

    copy frontend layout to backend

---
+++ backend/views/layouts/left-user.php
@@ -0,0 +1,115 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+            </div>
+            <div class="pull-left info">
+                <p>
+					<?= Yii::$app->user->identity->username; ?>
+				</p>
+
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <!-- search form ->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+				<span class="input-group-btn">
+					<button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+					</button>
+				</span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+		<?=
+
+		dmstr\widgets\Menu::widget(
+			[
+				'options'	 => ['class' => 'sidebar-menu'],
+				'items'		 => [
+					['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+					[
+						'label'	 => 'Master',
+						'icon'	 => 'fa fa-file',
+						'url'	 => '#',
+						'items'	 => [
+							[
+								'label'	 => 'Religion',
+								'icon'	 => 'fa fa-file-code-o',
+								'url'	 => ['/religion'],
+							],
+						],
+					],
+					[
+						'label'	 => 'Region',
+						'icon'	 => 'fa fa-share',
+						'url'	 => '#',
+						'items'	 => [
+							[
+								'label'	 => 'Country',
+								'icon'	 => 'fa fa-file-code-o',
+								'url'	 => ['/rgn-country'],
+							],
+							[
+								'label'	 => 'Province',
+								'icon'	 => 'fa fa-file-code-o',
+								'url'	 => ['/rgn-province'],
+							],
+							[
+								'label'	 => 'City',
+								'icon'	 => 'fa fa-file-code-o',
+								'url'	 => ['/rgn-city'],
+							],
+							[
+								'label'	 => 'District',
+								'icon'	 => 'fa fa-file-code-o',
+								'url'	 => ['/rgn-district'],
+							],
+							[
+								'label'	 => 'Subdistrict',
+								'icon'	 => 'fa fa-file-code-o',
+								'url'	 => ['/rgn-subdistrict'],
+							],
+							[
+								'label'	 => 'Postcode',
+								'icon'	 => 'fa fa-file-code-o',
+								'url'	 => ['/rgn-postcode'],
+							],
+						],
+					],
+					[
+						'label'	 => 'Modules',
+						'icon'	 => 'fa fa-share',
+						'url'	 => '#',
+						'items'	 => [
+							[
+								'label'	 => 'Region',
+								'icon'	 => 'fa fa-circle-o',
+								'url'	 => '/region',
+								'items'	 => [
+									['label' => 'Country', 'icon' => 'fa fa-circle-o', 'url' => ['/region/country'],],
+									['label' => 'Province', 'icon' => 'fa fa-circle-o', 'url' => ['/region/province'],],
+									['label' => 'City', 'icon' => 'fa fa-circle-o', 'url' => ['/region/city'],],
+									['label' => 'District', 'icon' => 'fa fa-circle-o', 'url' => ['/region/district'],],
+									['label' => 'Subdistrict', 'icon' => 'fa fa-circle-o', 'url' => ['/region/subdistrict'],],
+									['label' => 'Postcode', 'icon' => 'fa fa-circle-o', 'url' => ['/region/postcode'],],
+								],
+							],
+						],
+					],
+				],
+			]
+		)
+
+		?>
+
+    </section>
+
+</aside>
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,53 +1,205 @@
 <?php
 
+use yii\helpers\Html;
+
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Yii 2 Boilerplate';
+
 ?>
+<style>
+	dd {
+		margin-bottom: 30px;
+	}
+	dt {
+		margin-top: 20px;
+	}
+
+</style>
 <div class="site-index">
 
+	<div
+		class="fb-like"
+		data-share="true"
+		data-width="450"
+		data-show-faces="true"
+		style="float: right;"
+		>
+	</div>
+
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Yii 2 Boilerplate</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Template aplikasi dengan Framework Yii 2. Dilengkapi dengan database wilayah indonesia.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p>
+			<a class="btn btn-lg btn-success" href="http://github.com/fredyns/yii2-boilerplate">GitHub Project</a>
+			<a class="btn btn-lg btn-primary" href="http://fredyns.net/blog/2016/01/yii2-boilerplate/">Blog Post</a>
+		</p>
+
     </div>
 
     <div class="body-content">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+		<?php if (Yii::$app->user->isGuest): ?>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+			<p align="center">
+				Silahkan login/register dulu untuk melihat fitur lainnya. Gratis kok.
+				Untuk demo fitur user management.
+			</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+			<p align="center">
+				<?= Html::a('Login', ['/user/security/login'], ['class' => 'btn btn-lg btn-info', 'title' => 'masuk ke sistem']) ?>
+				&nbsp;
+				<?= Html::a('Lupa Password', ['/user/recovery/request'], ['class' => 'btn btn-lg btn-warning', 'title' => 'reset password']) ?>
+			</p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+			<p align="center">
+				<?= Html::a('Register', ['/user/registration/register'], ['class' => 'btn btn-lg btn-info', 'title' => 'mendaftarkan akun baru']) ?>
+				&nbsp;
+				<?= Html::a('Resend Confirmation', ['/user/registration/resend'], ['class' => 'btn btn-lg btn-info', 'title' => 'kirim ulang email konfirmasi akun']) ?>
+			</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+		<?php else: ?>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+			Welcome :)
+			<br/>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
+			<p style="text-align: justify;">
+				<a href="https://github.com/fredyns/yii2-boilerplate" target="_blank"><strong>Yii2-Boilerplate</strong></a> ini berisi:
+			</p>
+
+			<ul>
+				<li style="text-align: justify;">
+					<a href="https://github.com/yiisoft/yii2-app-advanced" target="_blank"><strong>Yii2-app-advance</strong></a>
+				</li>
+				<li style="text-align: justify;">
+					<a href="https://github.com/dektrium/yii2-user" target="_blank"><strong>Dektrium User Management</strong></a>
+				</li>
+				<li style="text-align: justify;">
+					<a href="https://github.com/dmstr/yii2-adminlte-asset" target="_blank"><strong>adminLTE</strong></a>
+				</li>
+				<li style="text-align: justify;">
+					<a href="https://github.com/cahyadsn/daerah" target="_blank">Database wilayah Indonesia</a>.
+				</li>
+			</ul>
+			&nbsp;
+
+			<strong>Tambahan Pribadi</strong>
+
+			Dari saya ada bebera class, fungsi serta extend dari komponen bawaan. Tapi yg utama ada 2, yaitu:
+
+			<dl>
+
+				<dt>1. Access Control tiap Model</dt>
+				<dd>disiapkan class untuk mengatur akses ke model, operasi yg tersedia untuk model serta link generatornya.</dd>
+
+				<dt>2. Module Database Daerah</dt>
+				<dd>biar tau aja bedanya komponen yg ditulis langsung di bawah frontend dan dikelompokkan dalam module.</dd>
+
+			</dl>
+
+			<p style="text-align: justify;">
+				Proses instalasi bisa lihat di
+				<a href="goo.gl/U1jZs6" target="_blank"><strong>blog ini</strong></a>.
+			</p>
+
+			<p style="text-align: justify;">
+				Selamat mencoba, semoga bermanfaat.
+				nanti klo ada update saya kabari lagi.
+				Jangan sungkan untuk komen ya.
+				:)
+			</p>
+
+			<!--
+			<p>
+				Klik menu di sebelah kiri untuk explore menu lainnya.
+			</p>
+
+			<strong>Proses Instalasi</strong>
+
+			langsung saja buka projectnya di <a href="https://github.com/fredyns/yii2-boilerplate" target="_blank">GitHub</a>. lalu langkahnya sbb:
+
+			<dl>
+
+				<dt>1. clone repo</dt>
+				<dd>
+					bisa download zip, via git client atau <a href="http://fredyns.net/blog/2016/01/clone-github-project-via-netbeans/" target="_blank">menggunakan NetBeans</a>.
+				</dd>
+
+				<dt>
+				2. install komponen via <a href="https://getcomposer.org/" target="_blank">composer</a>
+				</dt>
+				<dd>
+					buka terminal. masuk ke folder project. ketikan perintah :
+					<pre class="brush: shell; gutter: true">composer install</pre>
+				</dd>
+
+				<dt>3. memulai project Yii</dt>
+				<dd>
+					masih di terminal. ketik perintah:
+					<pre class="brush: shell; gutter: true">./yii init</pre>
+					<p style="text-align: justify;">bila di linux pastikan file "yii" bisa dieksekusi. Pilih environtment. Klo bingung pilih aja 'development' (masih dalam pengembangan)</p>
+				</dd>
+
+				<dt>4. Buat Database</dt>
+				<dd>
+					masuk mysql/phpmyadmin, lalu buat database untuk sistem.
+				</dd>
+
+				<dt>5. Set database connetion</dt>
+				<dd>
+					edit file <span style="color: #0000ff;">common/config/main-local.php</span>. ubah parameter koneksi database yg tertera disitu sesuai konfigurasi mysql Kamu.</p>
+				</dd>
+
+				<dt>6. migrasi database</dt>
+				<dd>
+					kembali ke terminal untuk setup tabel sistem di database. ketikan perintah berikut:
+					<pre class="brush: shell; gutter: true">./yii migrate/up --migrationPath=@vendor/dektrium/yii2-user/migrations</pre>
+					di boilerplate ini pake user management <span style="color: #0000ff;">dektrium/yii2-user</span>.
+
+					sampai sini harusnya sistem sudah bisa diakses lewat browser.
+				</dd>
+
+				<dt>7. Setup tabel lainnya</dt>
+				<dd>
+					Biar ringkas saya sarankan pake <a href="https://dev.mysql.com/downloads/workbench/" target="_blank">Workbench</a> , tak ada migration. Buka saja filenya <span style="color: #0000ff;">database/model.mwb</span> lalu sinkronkan dengan mysql kamu. Nama databasenya diatur sesuai yg dibuat tadi. Nanti struktur tabel di workbench bakal pindah ke mysql.
+					Kalau tak ada workbench, saya sediakan changelog sql yg bisa diimpor ke mysql di folder <span style="color: #0000ff;">database/changelog</span>. impor saja sesuai urutan.</p>
+				</dd>
+
+				<dt>8. Impor data</dt>
+				<dd>
+					database wilayah indonesia tersedia di folder <span style="color: #0000ff;">database/dump</span>. Diimpor ke mysql urut dari file:
+
+					<ol>
+						<li>region_country.sql</li>
+						<li>region_province.sql</li>
+						<li>region_city.sql</li>
+						<li>region_district.sql</li>
+						<li>region_subdistrict.sql</li>
+					</ol>
+
+					<p style="text-align: justify;">harus urut karena ada constrain relasi tabel. jadi harus urut dr negara lalu turun sampai kelurahan.</p>
+				</dd>
+
+			</dl>
+
+			<p style="text-align: justify;">
+				<b>Selesai!</b><br/>
+				coba cek lewat browser. eksplor semua menu yg tersedia di panel kiri .
+				contoh yg sudah jadi ada <a href="http://goo.gl/RoGYTv" target="_blank">disini</a>.
+			</p>
+
+			<p style="text-align: justify;">
+				Selamat mencoba, semoga bermanfaat.
+				nanti klo ada update saya kabari lagi.
+				Jangan sungkan untuk komen ya.
+				:)
+			</p>
+			-->
+
+		<?php endif; ?>
 
     </div>
 </div>
+
